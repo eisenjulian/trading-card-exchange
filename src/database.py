a1857227624d201d121d369d0730389f89b07e55
NAUSICAA\Julian <eisenjulian@gmail.com>
2018-06-28 10:47:59 -0300

    json dump

--- src/database.py
+++ src/database.py
@@ -1,10 +1,11 @@
 import redis
+import json
 import os
 
 db = redis.from_url(os.environ.get("REDIS_URL"))
 
 def get_user(id):
-    return db.get('user:' + id) or {}
+    return json.loads(db.get('user:' + id) or '{}')
 
 def set_user(user):
-    db.set('user:' + user['id'], user)
+    db.set('user:' + user['id'], json.dumps(user))
