ab97a135653e9e876a5ef2d44f35c0bb840ec52e
NAUSICAA\Julian <eisenjulian@gmail.com>
2018-06-28 10:40:58 -0300

    Save user data to db

--- src/database.py
+++ src/database.py
@@ -1,3 +1,10 @@
 import redis
+import os
 
-db = redis.from_url(os.environ.get("REDIS_URL"))
+db = redis.from_url(os.environ.get("REDIS_URL")`)
+
+def get_user(id):
+    return db.get('user:' + id) or {}
+
+def set_user(user):
+    db.set('user:' + user['id'], user)
