6a511f9ca83033e7fb572892afb2bf10300902fb
NAUSICAA\Julian <eisenjulian@gmail.com>
2018-06-28 10:43:24 -0300

    Typo

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 import redis
 import os
 
-db = redis.from_url(os.environ.get("REDIS_URL")`)
+db = redis.from_url(os.environ.get("REDIS_URL"))
 
 def get_user(id):
     return db.get('user:' + id) or {}
