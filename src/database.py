36e31dea32789f63cbf71aa59f11eb1d917236fd
quimeyv <quimeyv@gmail.com>
2018-07-04 22:59:52 -0300

    Refactor with new redis structure

--- src/database.py
+++ src/database.py
@@ -1,14 +1,18 @@
 import os
 import json
 import redis
+from hashlib import md5
 
 
 db = redis.from_url(os.environ.get("REDIS_URL"))
 
-WANTED = 'wanted'
-OWNERS = 'owners'
-PUT = 'put'
-GET = 'get'
+
+def get_users():
+    return [key.split(':')[1] for key in db.keys('user:*')]
+
+
+def get_cards():
+    return [key.split(':')[1] for key in db.keys('card:*')]
 
 
 def get_user(id):
@@ -19,29 +23,24 @@ def set_user(user):
     db.set('user:' + user['id'], json.dumps(user))
 
 
+def get_card(id):
+    return json.loads(db.get('card:' + id) or '{}')
+
+
+def set_card(card):
+    db.set('card:' + card['id'], json.dumps(card))
+
+
 def add_wanted(user, wanted):
     for card in wanted:
         add_one_wanted(user, card)
 
 
-def add_one_wanted(user, card):
-    db.hset(get_wanted_key(user), card, 1)
-
-
-def get_wanted_key(user):
-    return '{}:{}'.format(WANTED, user)
-
-
-def get_owners_key(card):
-    return '{}:{}'.format(OWNERS, card)
-
-
-def get_put_key(user):
-    return '{}:{}'.format(user, PUT)
-
-
-def get_get_key(user):
-    return '{}:{}'.format(user, GET)
+def add_one_wanted(user, card_id):
+    user['wanted'].append(card_id)
+    card = get_card(card_id)
+    card['whished'].append(user['id'])
+    set_card(card)
 
 
 def add_collection(user, collection):
@@ -49,13 +48,33 @@ def add_collection(user, collection):
         add_one_collection(user, card)
 
 
-def add_one_collection(user, card):
-    db.hset(get_owners_key(card), user, 1)
+def add_one_collection(user, card_id):
+    user['collection'].append(card_id)
+    card = get_card(card_id)
+    card['owners'].append(user['id'])
+    set_card(card)
 
 
-def get_keys_for_wanted():
-    return db.keys('{}:*'.format(WANTED))
+def add_transaction(transaction):
+    transaction_id = md5(json.dumps(transaction)).hexdigest
+    db.set('transaction:{}'.format(transaction_id), json.dumps(transaction))
+    for i in xrange(0, len(transaction), 2):
+        user_id = transaction[i]
+        card_to_get_id = transaction[i + 1]
+        card_to_put_id = transaction[i - 1]
+        add_user_transaction(
+            get_user(user_id), card_to_put_id, card_to_get_id, transaction_id
+        )
 
 
-def get_keys_for_owners():
-    return db.keys('{}:*'.format(OWNERS))
+def add_user_transaction(user, put, get, transaction_id):
+    user['wanted'].remove(get)
+    user['collection'].remove(put)
+    card_to_put = get_card(put)
+    card_to_put['owners'].remove(user['id'])
+    save_card(card_to_put)
+    card_to_get = get_card(get)
+    card_to_get['whished'].remove(user['id'])
+    save_card(card_to_get)
+    user['transactions'].append(dict(put=put, get=get, id=transaction_id))
+    set_user(user)
--- src/matching.py
+++ src/matching.py
@@ -1,69 +1,21 @@
 import networkx as nx
-import attr
-from collections import defaultdict
 import database as db
 
 
-@attr.s
-class Transaction(object):
-    put = attr.ib(default=None)
-    get = attr.ib(default=None)
-
-
-def run_transaction(user, card, ttype, success):
-    if ttype == db.GET:
-        if success:
-            db.db.hdel(db.get_get_key(user), card)
-        else:
-            db.db.hset(db.get_wanted_key(user), card, 1)
-    if ttype == db.PUT:
-        if success:
-            db.db.hdel(db.get_put_key(user), card)
-        else:
-            db.db.hset(db.get_owners_key(user), card, 1)
-    raise Exception('Error with transaction {} {} {} {}'.format(
-        user, card, ttype, success
-    ))
-
-
-def get_transactions_from_cycle(cycle):
-    transactions = defaultdict(Transaction)
-    for i, edge in enumerate(cycle):
-        if i % 2 == 0:
-            # user -> card
-            user, card = edge
-            transactions[user].put = card
-        if i % 2 == 1:
-            # card -> user
-            card, user = edge
-            transactions[user].get = card
-    return transactions
-
-
-def compute_match(start_user=None):
+def compute_match(start_user_id=None):
     graph = nx.DiGraph()
-    for key in db.get_keys_for_wanted():
-        _, user = key.split(':')
-        for card, _ in db.db.hgetall(key).iteritems():
-            graph.add_edge(user, card)
-    for key in db.get_keys_for_owners():
-        _, card = key.split(':')
-        for user, _ in db.db.hgetall(key).iteritems():
-            graph.add_edge(card, user)
-    if start_user is None:
+    for user_id in db.get_users():
+        user = db.get_user(user_id)
+        for card_id in user['wanted']:
+            graph.add_edge(user_id, card_id)
+    for card_id in db.get_cards():
+        card = db.get_card(card_id)
+        for user_id in card['owners']:
+            graph.add_edge(card_id, user_id)
+    if start_user_id is None:
         # TODO: Process output to generate exchanges
         return nx.simple_cycles(graph)
     try:
-        transactions = get_transactions_from_cycle(
-            nx.find_cycle(graph, start_user)
-        )
-        for user, transaction in transactions.iteritems():
-            # Put the transaction in db and mark the corresponding cards
-            # as unavailable
-            db.db.hset(db.get_put_key(user), transaction.put, 1)
-            db.db.hdel(db.get_owners_key(user), transaction.put)
-            db.db.hset(db.get_get_key(user), transaction.get, 1)
-            db.db.hdel(db.get_wanted_key(user), transaction.get)
-        return transactions
+        return db.add_transaction(nx.find_cycle(graph, start_user_id))
     except nx.exception.NetworkXNoCycle:
-        return defaultdict(Transaction)
+        return None
