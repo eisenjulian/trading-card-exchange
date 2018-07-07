f0557dff0501568d7fd953a89afd9a202018a4f7
quimeyv <quimeyv@gmail.com>
2018-07-07 20:32:05 -0300

    Rework transactions

--- src/database.py
+++ src/database.py
@@ -43,6 +43,10 @@ def set_card(card):
     db.set('card:' + card['id'], json.dumps(card))
 
 
+def is_user(id):
+    return int(id) > 1000
+
+
 def add_wanted(user, wanted):
     for card in wanted:
         add_one_wanted(user, card)
@@ -68,25 +72,31 @@ def add_one_collection(user, card_id):
 
 
 def add_transaction(transaction):
-    transaction_id = md5(json.dumps(transaction)).hexdigest
+    transaction_id = md5(json.dumps(transaction)).hexdigest()
     db.set('transaction:{}'.format(transaction_id), json.dumps(transaction))
-    for i in xrange(0, len(transaction), 2):
-        user_id = transaction[i]
-        card_to_get_id = transaction[i + 1]
-        card_to_put_id = transaction[i - 1]
+    offset = -1 if is_user(transaction[0]) else 0
+    for i in xrange(offset, len(transaction), 2):
+        user_id = transaction[i - 1]
+        card_to_get_id = transaction[i]
+        card_to_put_id = transaction[i - 2]
         add_user_transaction(
             get_user(user_id), card_to_put_id, card_to_get_id, transaction_id
         )
 
 
+def remove_if_exists(data, value):
+    if value in data:
+        data.remove(value)
+
+
 def add_user_transaction(user, put, get, transaction_id):
-    user['wanted'].remove(get)
-    user['collection'].remove(put)
+    remove_if_exists(user['wanted'], get)
+    remove_if_exists(user['collection'], put)
     card_to_put = get_card(put)
-    card_to_put['owners'].remove(user['id'])
+    remove_if_exists(card_to_put['owners'], user['id'])
     set_card(card_to_put)
     card_to_get = get_card(get)
-    card_to_get['whished'].remove(user['id'])
+    remove_if_exists(card_to_get['whished'], user['id'])
     set_card(card_to_get)
     user['transactions'].append(dict(put=put, get=get, id=transaction_id))
     set_user(user)
