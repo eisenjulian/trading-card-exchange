4cdc4468dd5661bbfd94c797ec7a921fa999e10e
quimeyv <quimeyv@gmail.com>
2018-07-08 17:48:02 -0300

    Translate trades

--- src/nlg.py
+++ src/nlg.py
@@ -67,9 +67,12 @@ def show_trades(t, trades):
                 "template_type": "generic",
                 "elements": [
                     {
-                        "title": "trade {} by {}".format(
-                            card_get.get('name', card_get['id']),
-                            card_put.get('name', card_put['id'])
+                        "title": t(
+                            'trade',
+                            dict(
+                                card_get=card_get.get('name', card_get['id']),
+                                card_put=card_put.get('name', card_put['id'])
+                            )
                         ),
                         "buttons": [button(t, '/cancel_transaction')]
                     }
