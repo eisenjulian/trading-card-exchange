8063f73feb1a3c90257a524fbc493edb7211f5a1
NAUSICAA\Julian <eisenjulian@gmail.com>
2018-07-09 15:14:24 -0300

    send batch messages

--- src/connectors/messenger_sender.py
+++ src/connectors/messenger_sender.py
@@ -4,6 +4,20 @@ import requests
 import os
 from src.utils import log
 
+def send_batch_messages(messages):
+    params = {"access_token": os.environ["PAGE_ACCESS_TOKEN"]}
+    headers = {"Content-Type": "application/json"}
+    for user_id, messages in messages.iteritems():
+        for message in messages:
+            data = json.dumps({
+                "recipient": {'id': user_id},
+                "message": message
+            })
+            r = requests.post("https://graph.facebook.com/v3.0/me/messages", params=params, headers=headers, data=data)
+            if r.status_code != 200:
+                log(r.status_code)
+                log(r.text)
+
 def send_messages(recipient, messages):
     log(u"sending message to {recipient}: {text}".format(recipient=recipient['id'], text=json.dumps(messages)))
     params = {"access_token": os.environ["PAGE_ACCESS_TOKEN"]}
