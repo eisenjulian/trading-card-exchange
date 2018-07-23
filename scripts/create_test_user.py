a926b2602565a1460587d09cb5b17c7ea609569d
quimeyv <quimeyv@gmail.com>
2018-07-23 07:30:46 -0300

    New create test user

--- scripts/create_test_user.py
+++ scripts/create_test_user.py
@@ -1,11 +1,13 @@
 import random
 import src.database as db
 
+for i in range(10):
+    user_id = str(random.randint(10 ** 6, 2 * 10 ** 6))
 
-user_id = str(random.randint(10 ** 6, 2 * 10 ** 6))
+    user = db.get_user(user_id)
+    user['id'] = user_id
 
-user = db.get_user(user_id)
-user['id'] = user_id
-
-db.add_wanted(user, map(str, range(1, 101)))
-db.add_collection(user, map(str, range(101, 201)))
+    cards = ['1104', '1136', '1163']
+    random.shuffle(cards)
+    db.add_wanted(user, cards[:1])
+    db.add_collection(user, cards[1:])
