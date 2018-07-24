30f5af6cdef57ea55b87e997a032eb025333f705
quimeyv <quimeyv@gmail.com>
2018-07-24 07:42:55 -0300

    Names for test users

--- scripts/create_test_user.py
+++ scripts/create_test_user.py
@@ -1,11 +1,18 @@
 import random
 import src.database as db
 
-for i in range(10):
+names = ['Mark', 'Carlos', 'Jose', 'Laura', 'Susan', 'Angela', 'John', 'Jane']
+last_names = ['Fernandez', 'Smith', 'Rodriguez', 'Doe', 'Brown', 'Davis']
+
+
+for i in range(50):
     user_id = str(random.randint(10 ** 6, 2 * 10 ** 6))
 
     user = db.get_user(user_id)
     user['id'] = user_id
+    user['first_name'] = random.choice(names)
+    user['last_name'] = random.choice(last_names)
+    user['tester'] = True
 
     cards = ['1104', '1136', '1163']
     random.shuffle(cards)
