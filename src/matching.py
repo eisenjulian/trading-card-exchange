6ee7dc2105f4059f1d06c4e189759bdf04ba1d76
quimeyv <quimeyv@gmail.com>
2018-07-24 07:44:51 -0300

    Remove card objects from matching

--- src/matching.py
+++ src/matching.py
@@ -9,9 +9,7 @@ def compute_match(start_user_id=None):
         user = db.get_user(user_id)
         for card_id in user['wanted']:
             graph.add_edge(user_id, card_id)
-    for card_id in db.get_cards():
-        card = db.get_card(card_id)
-        for user_id in card['owners']:
+        for card_id in user['collection']:
             graph.add_edge(card_id, user_id)
     cycles = nx.simple_cycles(graph)
     if start_user_id is None:
